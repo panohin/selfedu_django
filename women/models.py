c7aae6a0000ec7eeebb1497737f174d67d5b13ec
Pavel <retyui55@gmail.com>
2022-04-12 08:24:38 +0300

    Admin Panel

--- women/models.py
+++ women/models.py
@@ -3,13 +3,14 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    title = models.CharField(max_length=255, verbose_name='Название статьи')
+    content = models.TextField(blank=True, verbose_name='Содержание статьи')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, null=True, verbose_name='Рубрика')
+
 
     def __str__(self):
         return self.title
@@ -19,11 +20,12 @@ class Women(models.Model):
 
     class Meta:
         ordering = ['-time_update']
+        verbose_name = 'Женщины'
+        verbose_name_plural = 'Известные женщины'
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=250)
-    # women = models.ManyToManyField(Women, related_name='category', blank=True)
+    name = models.CharField(max_length=250, verbose_name="Наименование рубрики")
 
     def __str__(self):
         return self.name
@@ -32,4 +34,6 @@ class Category(models.Model):
         return reverse('by_category_url', kwargs={'cat_id':self.pk})
 
     class Meta:
-        ordering = ['name']
+        ordering = ['id']
+        verbose_name = "Рубрика"
+        verbose_name_plural = "Рубрики"
